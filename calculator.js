c459b83ea4e737ca1ef5dd66710f5f7c04565f6b
unknown <egbukwuprince@gmail.com>
2020-03-04 14:43:34 -0700

    Html practise list

---
+++ calculator.js
@@ -0,0 +1,7 @@
+let first = document.getElementById("firstNumber");
+      let second = document.getElementById("secondNumber");
+
+      function sum(a, b) {
+        return a + b;
+      }
+      
